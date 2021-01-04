e4f67283c6a67a9066b4bad006d5854e3d677b29
Calin Dragos George <calindragosgeorge@gmail.com>
2021-01-04 14:32:19 +0200

    The initial commit

--- rosserial_arduino/nodes/serial_node.py
+++ rosserial_arduino/nodes/serial_node.py
@@ -46,7 +46,7 @@ if __name__=="__main__":
     rospy.loginfo("ROS Serial Python Node")
 
     port_name = rospy.get_param('~port','/dev/ttyUSB0')
-    baud = int(rospy.get_param('~baud','57600'))
+    baud = int(rospy.get_param('~baud','115200'))
 
     # Number of seconds of sync failure after which Arduino is auto-reset.
     # 0 = no timeout, auto-reset disabled
